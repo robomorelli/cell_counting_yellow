324e5aac06673c33946d43e7c1044c5340f619cd
roberto <rmorelli@bo.infn.it>
2020-11-30 10:23:07 +0100

    020_cropper_yellow python script added

---
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,72 @@
+import numpy as np
+import imageio
+import cv2
+
+
+def cropper(image, mask):
+    
+    
+    CroppedImgs = np.zeros((NumCropped, YCropSize, XCropSize, 3), np.uint8)
+    CroppedMasks = np.zeros((NumCropped, YCropSize, XCropSize), np.uint8)
+    idx = 0
+    
+    for i in range(0, 4):
+        for j in range(0, 3):
+                
+                if (i == 0) & (j == 0):
+                    CroppedImgs[idx] = image[y_coord[j]:y_coord[j+1] + YShift, x_coord[i]:x_coord[i+1] + XShift]
+                    CroppedMasks[idx] = mask[y_coord[j]:y_coord[j+1] + YShift, x_coord[i]:x_coord[i+1] + XShift]
+                    idx +=1 
+
+                if (i == 0) & (j != 0):
+                    CroppedImgs[idx] = image[y_coord[j] - YShift : y_coord[j+1], x_coord[i]:x_coord[i+1] + XShift]
+                    CroppedMasks[idx] = mask[y_coord[j] - YShift : y_coord[j+1], x_coord[i]:x_coord[i+1] + XShift]
+                    idx +=1 
+
+                if (i != 0) &  (j == 0):
+                    CroppedImgs[idx] = image[y_coord[j]:y_coord[j+1] + YShift, x_coord[i] - XShift :x_coord[i+1]]
+                    CroppedMasks[idx] = mask[y_coord[j]:y_coord[j+1] + YShift, x_coord[i] - XShift :x_coord[i+1]]
+                    idx +=1 
+
+                if (i != 0) &  (j != 0):
+                    CroppedImgs[idx] = image[y_coord[j] - YShift : y_coord[j+1], x_coord[i] - XShift :x_coord[i+1]]
+                    CroppedMasks[idx] = mask[y_coord[j] - YShift : y_coord[j+1], x_coord[i] - XShift :x_coord[i+1]]
+                    idx +=1   
+            
+    return CroppedImgs, CroppedMasks
+
+def read_image_labels(image_id):
+     
+        x = cv2.imread(LoadImagesForCrop + image_id)
+        image = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(LoadLabelsForCrop + image_id)
+        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+        
+        return image, mask
+
+def make_cropper(image_ids, SaveCropImages, SaveCropMasks, shift = 0):
+    ix = shift
+
+    for ax_index, name in tqdm(enumerate(image_ids),total=len(image_ids)):
+        
+#         if name == '252.tiff':
+#             print(ix)    
+        image, mask = read_image_labels(name) 
+        if int(name.split('.')[0]) <= 252:
+            mask = erosion(np.squeeze(mask[:,:,0:1]), selem=np.ones([2,2]))
+        else:
+             mask = np.squeeze(mask[:,:,0:1])
+            
+       
+        CroppedImages, CroppedMasks = cropper(image, mask)                                     
+        
+        for i in range(0,NumCropped):
+            
+            crop_imgs_dir = SaveCropImages + '{}.tiff'.format(ix)
+            crop_masks_dir = SaveCropMasks + '{}.tiff'.format(ix) 
+
+            plt.imsave(fname= crop_imgs_dir, arr = CroppedImages[i])
+            plt.imsave(fname= crop_masks_dir,arr = CroppedMasks[i], cmap='gray')
+
+            ix +=1
+    return
---
+++ 020_cropper_yellow.py
@@ -0,0 +1,54 @@
+import glob
+import sys
+import numpy as np
+import imageio
+import cv2
+import random
+from skimage import transform
+import os
+from tqdm import tqdm
+from subprocess import check_output
+import matplotlib.pyplot as plt
+from importlib import import_module
+import pickle
+# from numba import jit
+from skimage.morphology import erosion
+from utils import *
+
+
+LoadImagesForCrop =  'DATA/TRAIN_VAL/all_images/images/'
+LoadLabelsForCrop = 'DATA/TRAIN_VAL/all_masks/masks/'
+SaveCropImages =  'DATA/TRAIN_VAL/all_cropped_images/images/' 
+SaveCropMasks = 'DATA/TRAIN_VAL/all_cropped_masks/masks/'
+
+IMG_WIDTH = 1600
+IMG_HEIGTH = 1200
+# Crop size 
+XCropSize = 512
+YCropSize = 512
+# if the coord are lesser than the crop size
+# overlapping between crop is allowed
+# if XCropCoord = XCropSize and same for Y coord, no overlapping beetween crop
+XCropCoord = 400
+YCropCoord = 400
+
+XCropNum = int(IMG_WIDTH/XCropCoord)
+YCropNum = int(IMG_HEIGTH/YCropCoord)
+
+NumCropped = int(IMG_WIDTH/XCropCoord * IMG_HEIGTH/YCropCoord)
+
+YShift = YCropSize - YCropCoord
+XShift = XCropSize - XCropCoord
+
+x_coord = [XCropCoord*i for i in range(0, XCropNum+1)]
+y_coord = [YCropCoord*i for i in range(0, YCropNum+1)]
+
+image_ids = os.listdir(LoadImagesForCrop)
+Number = [int(num.split('.')[0]) for num in image_ids]
+Number.sort()
+
+image_ids = [str(num) + '.tiff' for num in Number]
+
+if __name__ == "__main__":
+    
+    make_cropper(image_ids, SaveCropImages,SaveCropMasks)
