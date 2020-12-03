fdc9103b7afbe3042c16788813096cbb4637f649
m.dalla <m.dalla@cs.ucc.ie>
2020-12-03 18:32:21 +0100

    Fixing path in config and 010 and 011

--- 010_join_images.py
+++ 010_join_images.py
@@ -1,49 +1,36 @@
 import numpy as np
 import os
-from pathlib import Path
 from tqdm import tqdm
 import cv2
 import matplotlib.pyplot as plt
 from skimage.morphology import remove_small_objects
+from config import *
 
-
-IMG_WIDTH = 1600
-IMG_HEIGHT = 1200
 IMG_CHANNELS = 3
-
-NumberTest = 60
-tot_num = len(os.listdir('DATASET/original_images/images/'))
 
 idx = 0
 
-
-PATH = 'DATASET/original_images/images/'
-PATH_Y = 'DATASET/original_masks/masks/'
-
-PATH_TRAIN = 'DATASET/all_images/images/'
-PATH_TRAIN_Y = 'DATASET/all_masks/masks/'
-
-names = os.listdir(PATH)
+names = os.listdir(OriginalImages)
 
 for ix, name in tqdm(enumerate(names), total=len(names)):
 
-    img_x = cv2.imread(PATH + name)
+    img_x = cv2.imread(OriginalImages + name)
     img_x = cv2.cvtColor(img_x, cv2.COLOR_BGR2RGB)
 
     if img_x is None:
         name_x = name.replace('TIF', 'tif')
-        img_x = cv2.imread(PATH + name_x)
+        img_x = cv2.imread(OriginalImages + name_x)
         img_x = cv2.cvtColor(img_x, cv2.COLOR_BGR2RGB)
 
     try:
 
-        img_y = cv2.imread(PATH_Y + name)
+        img_y = cv2.imread(OriginalMasks + name)
         img_y = cv2.cvtColor(img_y, cv2.COLOR_BGR2RGB)[:, :, 0:1]
 
     except:
 
         name_y = name.replace('TIF', 'tif')
-        img_y = cv2.imread(PATH_Y + name_y)
+        img_y = cv2.imread(OriginalMasks + name_y)
         img_y = cv2.cvtColor(img_y, cv2.COLOR_BGR2RGB)[:, :, 0:1]
 
     if len(np.unique(img_y)) > 2:
@@ -57,25 +44,22 @@ for ix, name in tqdm(enumerate(names), total=len(names)):
     img_y = remove_small_objects(img_y, min_size=15)
     img_y = img_y.astype(np.uint8) * 255
 
-    img_dir = PATH_TRAIN + '{}.tiff'.format(ix)
-    mask_dir = PATH_TRAIN_Y + '{}.tiff'.format(ix)
+    img_dir = AllImages + '{}.tiff'.format(ix)
+    mask_dir = AllMasks + '{}.tiff'.format(ix)
     plt.imsave(fname=img_dir, arr=np.squeeze(img_x))
     plt.imsave(fname=mask_dir, arr=np.squeeze(img_y), cmap='gray')
 
-NEW_IMAGES = Path('DATASET/new_images/images/')
-NEW_MASKS = Path('DATASET/new_masks/masks/')
-
-new_images = os.listdir(NEW_IMAGES)
-new_masks = os.listdir(NEW_MASKS)
+new_images = os.listdir(NewImages)
+new_masks = os.listdir(NewMasks)
 
 idx = 0
 shift = 252
 for im_name, mask_name in tqdm(zip(new_images, new_masks), total=len(new_images)):
 
     print(im_name)
-    img_x = cv2.imread(str(NEW_IMAGES) + '/' + im_name)
+    img_x = cv2.imread(str(NewImages) + '/' + im_name)
     img_x = cv2.cvtColor(img_x, cv2.COLOR_BGR2RGB)
-    img_y = cv2.imread(str(NEW_MASKS) + '/' + mask_name)
+    img_y = cv2.imread(str(NewMasks) + '/' + mask_name)
     img_y = cv2.cvtColor(img_y, cv2.COLOR_BGR2RGB)[:, :, 0:1]
 
     if len(np.unique(img_y)) > 2:
@@ -85,8 +69,8 @@ for im_name, mask_name in tqdm(zip(new_images, new_masks), total=len(new_images)
     img_y = remove_small_objects(img_y, min_size=15)
     img_y = img_y.astype(np.uint8) * 255
 
-    img_dir = PATH_TRAIN + '{}.tiff'.format(shift + idx)
-    mask_dir = PATH_TRAIN_Y + '{}.tiff'.format(shift + idx)
+    img_dir = AllImages + '{}.tiff'.format(shift + idx)
+    mask_dir = AllMasks + '{}.tiff'.format(shift + idx)
     plt.imsave(fname=img_dir, arr=np.squeeze(img_x))
     plt.imsave(fname=mask_dir, arr=np.squeeze(img_y), cmap='gray')
 
--- config.py
+++ config.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
 
 IMG_WIDTH = 1600
-IMG_HEIGTH = 1200
+IMG_HEIGHT = 1200
 
-LoadImagesForCrop =  'DATA/TRAIN_VAL/all_images/images/'
-LoadMasksForCrop = 'DATA/TRAIN_VAL/all_masks/masks/'
-SaveCropImages =  'DATA/TRAIN_VAL/all_cropped_images/images/' 
-SaveCropMasks = 'DATA/TRAIN_VAL/all_cropped_masks/masks/'
+OriginalImages = 'DATASET/original_images/images/'
+OriginalMasks = 'DATASET/original_masks/masks/'
 
-LoadImagesForAug =  './DATA/TRAIN_VAL/all_cropped_images/images/'
-LoadMasksForAug = './DATA/TRAIN_VAL/all_weighted_masks/masks/'
-SaveAugImages =  './DATA/TRAIN_VAL/all_cropped_images/images/'
-SaveAugMasks = './DATA/TRAIN_VAL/all_weighted_masks/masks/'
+NewImages = 'DATASET/new_images/images/'
+NewMasks = 'DATASET/new_masks/masks/'
 
-LoadMasksForWeight = './DATA/TRAIN_VAL/all_cropped_masks/masks/'
-SaveWeightMasks = './DATA/TRAIN_VAL/all_weighted_masks/masks/'
+# Temporary folder for the union of original and new images, final dataset that we are going to share is going to be in
+# DATASET/train_val/{images, masks}_before_crop and DATASET/test/all_{images,masks}
+AllImages = 'DATASET/all_images/images/'
+AllMasks = 'DATASET/all_masks/masks/'
+
+TrainValImages = 'DATASET/train_val/images_before_crop/images/'
+TrainValMasks = 'DATASET/train_val/masks_before_crop/masks/'
+
+TestImages = 'DATASET/test/all_images/images/'
+TestMasks = 'DATASET/test/all_masks/masks/'
+
+# Folder for all cropped images and masks
+CropImages = 'DATASET/all_cropped_images/images/'
+CropMasks = 'DATASET/all_cropped_masks/masks/'
+
+# Final folder where the images for train reside, already cropped and augmented and weighted
+# Cropped and augmented images are going to be read from the same folder
+AugImages = 'DATASET/train_val/all_images/images/'
+AugMasks = 'DATASET/train_val/all_masks/masks/'
