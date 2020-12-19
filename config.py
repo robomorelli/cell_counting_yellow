018d6af32d6023b29328744684aa4801f9b1feb3
roberto <rmorelli@bo.infn.it>
2020-12-19 22:41:20 +0100

    included requirements and path fix in config for cnaf run

--- config.py
+++ config.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 import os
+import subprocess
+from subprocess import PIPE
 
 IMG_WIDTH = 1600
 IMG_HEIGHT = 1200
 
-OriginalImages = 'DATASET/original_images/images/'
-OriginalMasks = 'DATASET/original_masks/masks/'
+try:
+    o = subprocess.run(["hostname"],  capture_output=True)
+    o = o.stdout
+    o = o.strip().decode('utf-8')
+except:
+    o = subprocess.run(["hostname"],  stdout=PIPE, stderr=PIPE)
+    o = o.stdout
+    o = o.strip().decode('utf-8')
+
+if 'cnaf' in o:
+    root = '/storage/gpfs_maestro/hpc/user/rmorellihpc/cell_counting_yellow/'
+else:
+    root = ''
+OriginalImages = root + 'DATASET/original_images/images/'
+OriginalMasks = root + 'DATASET/original_masks/masks/'
 
 if not os.path.exists(OriginalImages):
     os.makedirs(OriginalImages)
@@ -13,8 +28,8 @@ if not os.path.exists(OriginalImages):
 if not os.path.exists(OriginalMasks):
     os.makedirs(OriginalMasks)
 
-NewImages = 'DATASET/new_images/images/'
-NewMasks = 'DATASET/new_masks/masks/'
+NewImages = root + 'DATASET/new_images/images/'
+NewMasks = root + 'DATASET/new_masks/masks/'
 
 if not os.path.exists(NewImages):
     os.makedirs(NewImages)
@@ -24,8 +39,8 @@ if not os.path.exists(NewMasks):
 
 # Temporary folder for the union of original and new images, final dataset that we are going to share is going to be in
 # DATASET/train_val/{images, masks}_before_crop and DATASET/test/all_{images,masks}
-AllImages = 'DATASET/all_images/images/'
-AllMasks = 'DATASET/all_masks/masks/'
+AllImages = root + 'DATASET/all_images/images/'
+AllMasks = root + 'DATASET/all_masks/masks/'
 
 if not os.path.exists(AllImages):
     os.makedirs(AllImages)
@@ -34,8 +49,8 @@ if not os.path.exists(AllMasks):
     os.makedirs(AllMasks)
 
 #I'd like to change in this way
-TrainValImages = 'DATASET/train_val/full_size/images/'
-TrainValMasks = 'DATASET/train_val/full_size/masks/'
+TrainValImages = root + 'DATASET/train_val/full_size/images/'
+TrainValMasks = root + 'DATASET/train_val/full_size/masks/'
 
 if not os.path.exists(TrainValImages):
     os.makedirs(TrainValImages)
@@ -43,8 +58,8 @@ if not os.path.exists(TrainValImages):
 if not os.path.exists(TrainValMasks):
     os.makedirs(TrainValMasks)
 
-TestImages = 'DATASET/test/all_images/images/'
-TestMasks = 'DATASET/test/all_masks/masks/'
+TestImages = root + 'DATASET/test/all_images/images/'
+TestMasks = root + 'DATASET/test/all_masks/masks/'
 
 if not os.path.exists(TestImages):
     os.makedirs(TestImages)
@@ -53,9 +68,9 @@ if not os.path.exists(TestMasks):
     os.makedirs(TestMasks)
 
 #This should be only for train_val
-CropImages = 'DATASET/train_val/cropped/images/'
-CropMasks = 'DATASET/train_val/cropped/masks/'
-CropWeightedMasks = 'DATASET/train_val/cropped/weighted_masks/'
+CropImages = root + 'DATASET/train_val/cropped/images/'
+CropMasks = root + 'DATASET/train_val/cropped/masks/'
+CropWeightedMasks = root + 'DATASET/train_val/cropped/weighted_masks/'
 
 if not os.path.exists(CropImages):
     os.makedirs(CropImages)
@@ -67,8 +82,8 @@ if not os.path.exists(CropWeightedMasks):
 
 # Final folder where the images for train reside, already cropped and augmented and weighted
 # Cropped and augmented images are going to be read from the same folder
-AugCropImages = 'DATASET/train_val/crop_augmented/images/'
-AugCropMasks = 'DATASET/train_val/crop_augmented/masks/'
+AugCropImages = root + 'DATASET/train_val/crop_augmented/images/'
+AugCropMasks = root + 'DATASET/train_val/crop_augmented/masks/'
 
 if not os.path.exists(AugCropImages):
     os.makedirs(AugCropImages)
@@ -76,7 +91,7 @@ if not os.path.exists(AugCropImages):
 if not os.path.exists(AugCropMasks):
     os.makedirs(AugCropMasks)
 
-ModelResults = 'model_results/'
+ModelResults = root + 'model_results/'
 
 if not os.path.exists(ModelResults):
     os.makedirs(ModelResults)
