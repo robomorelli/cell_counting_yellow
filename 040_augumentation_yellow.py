0f2f01f97ab083f42d7ab42151a0260cc3bc4c9d
roberto <rmorelli@bo.infn.it>
2020-12-19 10:22:31 +0100

    fix crop

--- 040_augumentation_yellow.py
+++ 040_augumentation_yellow.py
@@ -64,14 +64,14 @@ if __name__ == "__main__":
         print('starting augmentation')
 
     src_files = os.listdir(CropImages)
-    
+
     print('copying images')
     for file_name in src_files:
         full_file_name = os.path.join(CropImages, file_name)
         if os.path.isfile(full_file_name):
             shutil.copy(full_file_name, AugCropImages)
 
-
+    print('copying masks')
     src_files = os.listdir(CropWeightedMasks)
     for file_name in src_files:
         full_file_name = os.path.join(CropWeightedMasks, file_name)
