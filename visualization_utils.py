5e34add2a3e2a79548f4560dd0fc08c148d5612b
Luca Clissa <luca.clissa2@unibo.it>
2021-01-21 15:16:53 +0100

    Add functions draw_bounding_boxes_with_metrics and compare_predictions_with_metrics to visualization_utils.py as useful visualizations for comparing different models

--- visualization_utils.py
+++ visualization_utils.py
@@ -112,7 +112,174 @@ def plot_predicted_mask(model, test_img_path, test_masks_path, threshold, post_p
             #     break
 
     return(None)
-    
+
+
+def draw_bounding_boxes_with_metrics(img, pred_mask, mask):
+    '''Add bounding boxes for TP, FP, and FN to the original image.
+
+    Keyword arguments:
+    img -- array of the original image
+    pred_mask -- array of the predicted mask
+    mask -- groundtruth mask
+
+    Return: img, tp, fp, fn, ae, pred_rgb, true_count.
+    '''
+    pred_mask = pred_mask.astype("bool")
+    pred_label, pred_rgb = ndimage.label(pred_mask)
+    pred_objs = ndimage.find_objects(pred_label)
+
+    # extract target objects and counts
+    true_label, true_count = ndimage.label(mask)
+    true_objs = ndimage.find_objects(true_label)
+
+    # compute centers of predicted objects
+    pred_centers = []
+    for ob in pred_objs:
+        pred_centers.append(((int((ob[0].stop - ob[0].start) / 2) + ob[0].start),
+                             (int((ob[1].stop - ob[1].start) / 2) + ob[1].start)))
+
+    # compute centers of target objects
+    targ_center = []
+    for ob in true_objs:
+        targ_center.append(((int((ob[0].stop - ob[0].start) / 2) + ob[0].start),
+                            (int((ob[1].stop - ob[1].start) / 2) + ob[1].start)))
+
+    # associate matching objects, true positives
+    tp = 0
+    tp_objs = []
+
+    for pred_idx, pred_obj in enumerate(pred_objs):
+
+        min_dist = 31  # 1.5-cells distance is the maximum accepted
+        TP_flag = 0
+
+        for targ_idx, targ_obj in enumerate(true_objs):
+
+            dist = hypot(pred_centers[pred_idx][0] - targ_center[targ_idx][0],
+                         pred_centers[pred_idx][1] - targ_center[targ_idx][1])
+
+            if dist < min_dist:
+                TP_flag = 1
+                min_dist = dist
+                index_targ = targ_idx
+                index_pred = pred_idx
+
+        if TP_flag == 1:
+            tp += 1
+            TP_flag = 0
+
+            cv2.rectangle(img, (pred_objs[index_pred][1].start - 10, pred_objs[index_pred][0].start - 10),
+                          (pred_objs[index_pred][1].stop + 10, pred_objs[index_pred][0].stop + 10), (0, 255, 0), 3)
+
+            tp_objs.append(pred_objs[index_pred])
+            targ_center.pop(index_targ)
+            true_objs.pop(index_targ)
+
+    # derive false negatives and false positives
+    fp = 0
+    for pred_obj in pred_objs:
+        if pred_obj not in tp_objs:
+            cv2.rectangle(img, (pred_obj[1].start - 10, pred_obj[0].start - 10),
+                          (pred_obj[1].stop + 10, pred_obj[0].stop + 10), (255, 0, 0), 3)
+            fp += 1
+
+    fn = 0
+    for targ_obj in true_objs:
+        cv2.rectangle(img, (targ_obj[1].start - 10, targ_obj[0].start - 10),
+                      (targ_obj[1].stop + 10, targ_obj[0].stop + 10), (0, 0, 255), 3)
+        fn += 1
+
+    ae = abs(true_count - pred_rgb)
+    return (img, tp, fp, fn, ae, pred_rgb, true_count)
+
+
+def compare_predictions_with_metrics(models_dict, test_img_path, test_masks_path, threshold="best",
+                                     post_processing=True):
+    """Plot comparisons of all models in models_dict with bounding boxes for TP, FP, and FN.
+
+    :param models_dict: dictionary with structure {model name: model object}
+    :param test_img_path: path where original images are stored
+    :param test_masks_path: path where corresponding masks are stored
+    :param threshold: Cutoff for thresholding the prediction. values:
+                        - 'best' (default): it takes the best F1 threshold from eval metrics
+                        - float between 0 and 1.
+    :param post_processing: boolean for post-processing (default: True)
+    :return: None
+    """
+    import pandas as pd
+    import numpy as np
+    from matplotlib import pyplot as plt
+    from pathlib import Path
+    from evaluation_utils import mask_post_processing
+
+    import cv2
+    import matplotlib.patches as mpatches
+
+    # repo_path = Path("/storage/gpfs_maestro/hpc/user/rmorellihpc/cell_counting_yellow")
+    repo_path = Path("/home/luca/PycharmProjects/cell_counting_yellow")
+
+    legend_background_color = 'white'
+    line_thickness = 1.5
+    # counter=0
+    for idx, img_path in enumerate(test_img_path.iterdir()):
+
+        if not img_path.name.startswith("aug_"):
+            # fig, axes = plt.subplots(int(np.ceil(len(models_dict) / 2)), 2, figsize=(20, 6))
+            fig, axes = plt.subplots(1,len(models_dict), figsize=(20, 6))
+            # fig.suptitle(img_path.name, fontsize=22)
+            print("\033[31m" + img_path.name)
+
+            mask_path = test_masks_path / img_path.name
+            mask = cv2.imread(str(mask_path), cv2.IMREAD_GRAYSCALE)
+
+            # predictions
+            for idx, model_item in enumerate(models_dict.items()):
+                model_name, model = model_item[0], model_item[1]
+                img_rgb = cv2.imread(str(img_path), cv2.IMREAD_COLOR)
+                img_rgb = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2RGB)
+
+                # predictions
+                img_rgb = np.expand_dims(img_rgb, 0)
+                pred_mask_rgb = np.squeeze(model.predict(img_rgb / 255.))
+                if type(threshold) != float:
+                    opt_thresh_path = repo_path / "results/eval" / 'metrics_{}.csv'.format(model_name)
+                    df = pd.read_csv(opt_thresh_path, index_col='Threshold')
+                    cur_threshold = df.F1.idxmax()
+                else:
+                    cur_threshold = threshold
+                thresh_image = np.squeeze((pred_mask_rgb > cur_threshold).astype('uint8'))
+
+                # apply post-processing
+                if post_processing:
+                    thresh_image = mask_post_processing(thresh_image)
+
+                img, tp, fn, fp, ae, pred_rgb, true_count = draw_bounding_boxes_with_metrics(np.squeeze(img_rgb), thresh_image, mask)
+
+                # plot
+                axes[idx].imshow(img, cmap=plt.cm.RdBu)
+                tp_patch = mpatches.Circle((0.1, 0.1), 0.25, facecolor=legend_background_color,
+                                           edgecolor="green", linewidth=line_thickness)
+                fp_patch = mpatches.Circle((0.1, 0.1), 0.25, facecolor=legend_background_color,
+                                           edgecolor="red", linewidth=line_thickness)
+                fn_patch = mpatches.Circle((0.1, 0.1), 0.25, facecolor=legend_background_color,
+                                           edgecolor="blue", linewidth=line_thickness)
+                ae_patch = mpatches.Circle((0.1, 0.1), 0, facecolor=legend_background_color,
+                                           edgecolor=legend_background_color, linewidth=line_thickness)
+                title = "Predicted count: {} - True count: {}\n".format(pred_rgb, true_count) + r"$\bf{{{}}}$".format(model_name)
+                axes[idx].set_title(title, fontsize=18)
+
+                legend = axes[idx].legend([tp_patch, fp_patch, fn_patch, ae_patch],
+                                          ["True Positive: {}".format(tp), "False Positive: {}".format(fp),
+                                           "False Negative: {}".format(fn), "Absolute Error: {}".format(ae)],
+                                          bbox_to_anchor=(0.5, -0.3), loc='lower center', ncol=2, fontsize=14)
+                frame = legend.get_frame()
+                frame.set_color(legend_background_color)
+            plt.show()
+            # counter +=1
+            # if counter==5:
+            #     break
+    return (None)
+
 
 def plot_predictions_with_metrics(img, img_name, pred_mask, mask):
     '''Plot original image with bounding boxes for TP, FP, and FN.
@@ -125,7 +292,7 @@ def plot_predictions_with_metrics(img, img_name, pred_mask, mask):
     
     Return: None.
     '''
-    
+
     pred_mask = pred_mask.astype("bool")
 
     pred_label, pred_rgb = ndimage.label(pred_mask)
@@ -134,7 +301,7 @@ def plot_predictions_with_metrics(img, img_name, pred_mask, mask):
     # read mask and extract target objects and counts
     true_label, true_count = ndimage.label(mask)
     true_objs = ndimage.find_objects(true_label)
-    
+
     # compute centers of predicted objects
     pred_centers = []
     for ob in pred_objs:
@@ -171,7 +338,7 @@ def plot_predictions_with_metrics(img, img_name, pred_mask, mask):
         if TP_flag == 1:
             tp += 1
             TP_flag = 0
-            
+
             cv2.rectangle(img,(pred_objs[index_pred][1].start-10,pred_objs[index_pred][0].start-10),
                           (pred_objs[index_pred][1].stop+10,pred_objs[index_pred][0].stop+10),(0,255,0),3)
 
@@ -224,8 +391,8 @@ def plot_predictions_with_metrics(img, img_name, pred_mask, mask):
     frame.set_color(legend_background_color)
     plt.show()
     return(None)
-    
-    
+
+
 def plot_MAE(test_metrics):
     '''Plot mean absolute error distribution based on pandas dataframe. Return None.'''
     
